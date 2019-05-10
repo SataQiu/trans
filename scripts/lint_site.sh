b3ac997a001515ee04867823d7eb44a23ec33c68
SataQiu <qiushida@beyondcent.com>
2019-05-10 16:25:22 +0800

    fix

--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -7,24 +7,21 @@ mdspell --version
 echo -ne "mdl "
 mdl --version
 
-# This performs spell checking and style checking over markdown files in a content
-# directory. 
+# This performs spell checking and style checking over markdown files in a content directory. 
 check_content() {
 
-    regexp="[[:digit:]]\+$"
+    # only check pull request, skip others
+    if [[ -z $CIRCLE_PULL_REQUEST ]]; then
+        exit 0
+    fi
 
-    PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o $regexp`
-
+    # parse target branch
+    PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o "[[:digit:]]\+$"`
     url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$PR_NUMBER"
-
     target_branch=$(curl -s -X GET -G $url | jq '.base.ref' | tr -d '"')
 
-    echo "Resetting $target_branch to where the remote version is..."
-
     git checkout -q $target_branch
-
     git reset --hard -q origin/$target_branch
-
     git checkout -q $CIRCLE_BRANCH
 
     echo "Getting list of changed files..."

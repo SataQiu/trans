4e2623fad35e2d25e2d1dcf5cb07b6f42e355356
SataQiu <qiushida@beyondcent.com>
2019-05-10 17:37:22 +0800

    k

--- scripts/lint_site.sh
+++ scripts/lint_site.sh
@@ -12,59 +12,46 @@ check_content() {
 
     # only check pull request, skip others
     if [[ -z $CIRCLE_PULL_REQUEST ]]; then
-        echo "skip, only check pull request "
+        echo "Skip, only check pull request "
         exit 0
     fi
 
     # parse target branch
     url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER"
     target_branch=$(curl -s -X GET -G $url | jq '.base.ref' | tr -d '"')
-    circle_branch=$(curl -s -X GET -G $url | jq '.head.ref' | tr -d '"')
+    local_branch=$(curl -s -X GET -G $url | jq '.head.ref' | tr -d '"')
 
-    git checkout -b $circle_branch
-
+    # get changed files of this PR
+    git checkout -b $local_branch
     git checkout -q $target_branch
     git reset --hard -q origin/$target_branch
-
-    git branch
-    git checkout -q $circle_branch
+    git checkout -q $local_branch
 
     echo "Getting list of changed files..."
-    changed_files=$(git diff --name-only $target_branch..$circle_branch -- '*.md')
-
+    changed_files=$(git diff --name-only $target_branch..$local_branch -- '*.md')
     echo ${changed_files[@]}
 
-    exit 0
-
-    for month in $(ls 2019)
+    for file in ${changed_files}
     do  
-        if [[ ${month} -ge "05" ]]; then
-           mdspell en --ignore-acronyms --ignore-numbers --no-suggestions --report "2019/${month}/*/*.md"
+        # spell check
+        mdspell --en-us --ignore-acronyms --ignore-numbers --no-suggestions --report ${file}
+        if [[ "$?" != "0" ]]
+        then
+            FAILED=1
+        fi
+
+        # markdown format check
+        mdl --ignore-front-matter --style mdl_style.rb ${file}
+        if [[ "$?" != "0" ]]
+        then
+            FAILED=1
         fi
     done
-    
-    if [[ "$?" != "0" ]]
-    then
-        FAILED=1
-    fi
-
-    for month in $(ls 2019)
-    do  
-        if [[ ${month} -ge "05" ]]; then
-            mdl --ignore-front-matter --style mdl_style.rb "2019/${month}"
-        fi
-    done
-   
-    if [[ "$?" != "0" ]]
-    then
-        FAILED=1
-    fi
 }
 
-check_content . --en-us
+check_content
 
-if [[ ${FAILED} -eq 1 ]]
-then
+if [[ ${FAILED} -eq 1 ]]; then
     echo "LINTING FAILED"
     exit 1
 fi
